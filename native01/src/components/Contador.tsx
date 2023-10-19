16b2688e15c6c76e62a85b08f4c1e504e5fc42cd
jgmejiaco <jgmejiaco@gmail.com>
2023-10-19 18:45:29 -0500

    componente contador

--- native01/src/components/Contador.tsx
+++ native01/src/components/Contador.tsx
@@ -1,7 +1,32 @@
+import {useState} from "react";
+
 const Contador = () => {
+
+    const [valor, setValor] = useState(10);
+
+    const acumular = (numero: number) => {
+        setValor(valor + numero);
+    }
+
   return (
     <>
-        <h2>Contador</h2>
+        <h3>Contador: <small>{valor}</small></h3>
+
+        <button
+            className="btn btn-primary"
+            onClick={()=>acumular(1)}
+        >
+            +1
+        </button>
+
+        &nbsp;
+
+        <button
+            className="btn btn-primary"
+            onClick={()=>acumular(-1)}
+        >
+            -1
+        </button>
     </>
   )
 }
