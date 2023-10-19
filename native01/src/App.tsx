4b641dba85de0462ad5e8d1187d03c7523fbbc2d
jgmejiaco <jgmejiaco@gmail.com>
2023-10-19 15:32:00 -0500

    native01

---
+++ native01/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TiposBasicos from './typescript/TiposBasicos'
+
+const App = () => {
+  return (
+    <div className='mt-2'>
+      <h1>hola</h1>
+
+      <hr />
+
+      <TiposBasicos/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ native01/src/typescript/TiposBasicos.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TiposBasicos = () => {
+  return (
+    <>
+      <h1>Tipos Básicos TypeScript</h1>
+    </>
+  )
+}
+
+export default TiposBasicos
