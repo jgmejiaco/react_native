dd1c01572caab1db6b263b4cc571d2f694ba9417
jgmejiaco <jgmejiaco@gmail.com>
2023-10-26 16:14:04 -0500

    botones incremento y decremento

--- 03-contador/src/screens/ContadorScreen.tsx
+++ 03-contador/src/screens/ContadorScreen.tsx
@@ -1,19 +1,65 @@
 import React, { useState } from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 
 export const ContadorScreen = () => {
 
-  const [contador, setContador] = useState(10);
+  const [contador, setContador] = useState(1);
 
   return (
-    <View style={{flex:1, justifyContent:'center'}}>
-      <Text style={{fontSize:45,textAlign:'center',top:-15}}>
+    <View style={styles.container}>
+      <Text style={styles.title}>
         Contador: {contador}
       </Text>
 
-      <TouchableOpacity onPress={ () => setContador(contador + 1) }>
-        <Text>Contar</Text>
+      {/* ================================== */}
+
+      <TouchableOpacity onPress={ () => setContador(contador - 1) } style={styles.fabLocationBL}>
+        <View style={styles.fab}>
+        <Text style={styles.fabText}>-1</Text>
+        </View>
+      </TouchableOpacity>
+
+      {/* ============= */}
+
+      <TouchableOpacity onPress={ () => setContador(contador + 1) } style={styles.fabLocationBR}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
       </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent:'center'
+  },
+  title: {
+    textAlign:'center',
+    fontSize:45
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  fab: {
+    backgroundColor:'#5856d6',
+    width:60,
+    height:60,
+    borderRadius:100,
+    justifyContent: 'center'
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  }
+})
